683b6911bba59fe41db3ad66be063d63741ae09e
Tinh <nguuyenvantinhfithou@gmail.com>
2021-04-26 17:29:09 +0700

    Initial Commit

---
+++ PickerForMaster/FileSMS.swift
@@ -0,0 +1,15 @@
+//
+//  FileSMS.swift
+//  PickerForMaster
+//
+//  Created by Nguyen Van Tinh on 4/26/21.
+//
+
+import Foundation
+
+public class FileSMS{
+    public static func Check()
+    {
+        print("Ok demo")
+    }
+}
---
+++ PickerForMaster/PickerForMaster.h
@@ -0,0 +1,18 @@
+//
+//  PickerForMaster.h
+//  PickerForMaster
+//
+//  Created by Nguyen Van Tinh on 4/26/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PickerForMaster.
+FOUNDATION_EXPORT double PickerForMasterVersionNumber;
+
+//! Project version string for PickerForMaster.
+FOUNDATION_EXPORT const unsigned char PickerForMasterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PickerForMaster/PublicHeader.h>
+
+
